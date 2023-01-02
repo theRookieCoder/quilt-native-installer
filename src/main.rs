8866c618378ff8b5cd1b357e962ec079814390c8
Ilesh Thiada <ileshkt@gmail.com>
2023-01-02 19:51:26 +0530

    Fix merge conflict, slim dependencies, disable CLI

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
 #![windows_subsystem = "windows"]
 
-use clap::Parser;
-
-mod cli;
+// mod cli;
 mod gui;
 mod installer;
 
 const ICON: &[u8] = include_bytes!("../quilt.png");
 
 fn main() -> anyhow::Result<()> {
-    let args = cli::Args::parse();
+    // let args = cli::Args::parse();
 
-    if let Some(subcommand) = args.subcommand {
-        match subcommand {
-            cli::SubCommands::Client => println!("Installing client..."),
-            cli::SubCommands::Server => println!("Installing server..."),
-        }
-        std::thread::sleep(std::time::Duration::from_secs(2));
-        println!("Just kidding, we haven't implemented the CLI yet :)");
-    } else {
+    // if let Some(subcommand) = args.subcommand {
+    //     match subcommand {
+    //         cli::SubCommands::Client => todo!(),
+    //         cli::SubCommands::Server => todo!(),
+    //     }
+    // } else {
         gui::run()?;
-    }
+    // }
 
     Ok(())
 }
